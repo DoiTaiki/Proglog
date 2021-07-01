0c3883de5257835ec8e099087f7f1be51172d436
土肥　大樹 <bigtree19940729@gmail.com>
2021-07-01 19:55:24 +0900

    modify entrypoint_ci.sh 3

--- entrypoint_ci.sh
+++ entrypoint_ci.sh
@@ -5,6 +5,13 @@ set -e
 rm -f /proglog/tmp/pids/server.pid
 
 # # production environment's db settings(Is it needed only first time?)
+bundle exec rails dbconsole << EOF
+SELECT pg_terminate_backend(psa.pid)
+  FROM pg_stat_activity AS psa
+  WHERE psa.datname = 'Proglog' 
+    AND psa.pid <> pg_backend_pid();
+EOF
+
 RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop
 
 RAILS_ENV=production bundle exec rails db:create

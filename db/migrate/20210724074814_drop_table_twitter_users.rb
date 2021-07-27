ea9581d1d8cd752b0476ebf31c7ac334af7dc231
土肥　大樹 <bigtree19940729@gmail.com>
2021-07-27 19:12:22 +0900

    delete an unnecessary migration file

--- db/migrate/20210724074814_drop_table_twitter_users.rb
+++
@@ -1,9 +0,0 @@
-class DropTableTwitterUsers < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :twitter_users do |t|
-      t.string "uid", null: false
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
-    end
-  end
-end
